01a085b4b6f5e9333e8259d299c128df1b5ac740
François de Metz <francois@2metz.fr>
2023-07-03 21:33:47 +0200

    Remove findDistEsm

--- code/frameworks/ember/src/util.ts
+++ code/frameworks/ember/src/util.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 import { sync as findUpSync } from 'find-up';
 
-export const findDistEsm = (cwd: string, relativePath: string) => {
+export const findDistFile = (cwd: string, relativePath: string) => {
   const nearestPackageJson = findUpSync('package.json', { cwd });
   if (!nearestPackageJson) {
     throw new Error(`Could not find package.json in: ${cwd}`);
